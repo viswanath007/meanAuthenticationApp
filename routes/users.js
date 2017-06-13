8c8a97cbe574c7580c24de34c25f951e3be18a83
Admin <admin@Admins-Mac.local>
2017-06-13 12:08:07 +0530

    create DB model, setup user registration

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router  = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
-router.post('/registration', (req, res, next) => {
-  res.send('Registration');
+const User = require('../models/user');
+
+router.post('/register', (req, res, next) => {
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  User.addUser(newUser, (err, user) => {
+    if(err) {
+      res.json({success: false, msg: 'Failed to register'});
+    } else {
+      res.json({success: true, msg: 'you are registerd'});
+    }
+  });
 });
 
 router.post('/authenticate', (req, res, next) => {
