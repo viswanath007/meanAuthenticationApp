1357d07447742a5c95ec2622143cc8c8cdf906ed
Admin <admin@Admins-Mac.local>
2017-06-13 16:30:30 +0530

    Authentication by passport

--- app.js
+++ app.js
@@ -37,6 +37,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 // bodyparser 
 app.use(bodyParser.json());
 
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
 app.use('/users', users);
 
 // index route
